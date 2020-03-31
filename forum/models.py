90de64cdbfd85707459b9691c218b6440e4b5cbc
ravinduf <ravinduf@rocketmail.com>
2020-03-31 13:14:24 +0530

    added question model

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+class Question(models.Model):
+    Author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete = models.CASCADE)
+    Question = models.TextField()
+    Votes = models.SmallIntegerField(default = 0)
+
+    def __str__(self):
+        return self.Question
+
+    def vote(self):
+        self.Votes += 1
