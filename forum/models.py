4c8fe63e0b3b86119362a18e8e1d97f5ea9bd72a
ravinduf <ravinduf@rocketmail.com>
2020-04-01 13:46:32 +0530

    changed models.py

--- forum/models.py
+++ forum/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from django.conf import settings
+from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
     Author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete = models.CASCADE)
     Question = models.TextField()
     Votes = models.SmallIntegerField(default = 0)
-
+    added_date = models.DateTimeField()
+    
     def __str__(self):
         return self.Question
 
     def vote(self):
         self.Votes += 1
-        
