349fc90fa810a8632ab0b81f7ac5e2d989a4532d
ravinduf <ravinduf@rocketmail.com>
2020-04-01 14:01:07 +0530

    added home page

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
+def index(request):
+    questions = Question.objects.order_by('added_date').reverse()
+
+    return render(request, 'forum/home_page.html',{'questions' : questions })
