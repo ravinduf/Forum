92a4232c0927a4f4b8dcd149baad9f4aa3445fa0
ravinduf <ravinduf@rocketmail.com>
2020-04-06 16:11:47 +0530

    fixed to show answers with question question page

--- forum/models.py
+++ forum/models.py
@@ -3,26 +3,26 @@ from django.conf import settings
 from django.utils import timezone
 # Create your models here.
 class Question(models.Model):
-    Author = models.ForeignKey(settings.AUTH_USER_MODEL ,on_delete = models.CASCADE)
-    Question = models.TextField()
-    Votes = models.SmallIntegerField(default = 0)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL ,on_delete = models.CASCADE)
+    question = models.TextField()
+    votes = models.SmallIntegerField(default = 0)
     added_date = models.DateTimeField()
     
     def __str__(self):
-        return self.Question
+        return self.question
 
     def vote(self):
         self.Votes += 1
 
-class Answer(models.Model):
-    Question = models.ForeignKey('forum.Question' ,on_delete = models.CASCADE, related_name = 'answer')
-    Author = models.CharField(max_length=50)
-    Answer = models.TextField()
-    Votes = models.SmallIntegerField(default = 0)
+class answer(models.Model):
+    question = models.ForeignKey('forum.Question' ,on_delete = models.CASCADE, related_name = 'answer')
+    author = models.CharField(max_length=50)
+    answer = models.TextField()
+    votes = models.SmallIntegerField(default = 0)
     added_date = models.DateTimeField()
 
     def __str__(self):
-        return self.Answer
+        return self.answer
     
     def vote(self):
         self.Votes += 1
--- forum/urls.py
+++ forum/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name="home_page"),
-    path('/question/<int:pk>', views.question_info ,name = "question_details"),
+    path('question/<int:pk>', views.question_info ,name = "question_details"),
     
 ]
