b6dfbdfcfad4bcc56ff54bdd532e23238a1e0cb4
ravinduf <ravinduf@rocketmail.com>
2020-04-07 22:13:04 +0530

    added to increase votes and decrease

--- forum/urls.py
+++ forum/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index,name="home_page"),
     path('question/<int:pk>', views.question_info ,name = "question_details"),
+    path('addVote/<int:pk>', views.addVote, name = "addVote"),
+    path('subVote/<int:pk>', views.subVote, name = "subVote"),
+
     
 ]
--- forum/views.py
+++ forum/views.py
@@ -24,3 +24,16 @@ def question_info(request, pk):
         else:
             return HttpResponse('hello world')
     return render(request, 'forum/question_page.html',{ 'question': question , 'form': form} )
+
+def addVote(request,pk):
+    question = get_object_or_404 (Question, pk=pk)
+    question.votes += 1
+    question.save()
+    return redirect('question_details', pk = question.pk) #you have to give the urlpattern name instead of view function and 
+                                                        #always remember to give simillar names for them
+
+def subVote(request,pk):
+    question = get_object_or_404 (Question, pk=pk)
+    question.votes -= 1
+    question.save()
+    return redirect('question_details', pk = question.pk)
