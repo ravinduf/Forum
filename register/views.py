3fe32c86f0a4dcbb347ad3cbac72c77888c9536b
ravinduf <ravinduf@rocketmail.com>
2020-04-08 11:50:38 +0530

    fix auto login after registration

--- register/views.py
+++ register/views.py
@@ -8,7 +8,10 @@ def register(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
             return redirect('/')
 
     else:
