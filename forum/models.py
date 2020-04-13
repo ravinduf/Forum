9fec8620e750def2d9782df12dd9dc3304199a23
ravinduf <ravinduf@rocketmail.com>
2020-04-13 20:39:59 +0530

    error in voting system

--- forum/models.py
+++ forum/models.py
@@ -2,11 +2,20 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 # Create your models here.
+class Voter(models.Model):
+    name = models.CharField(max_length = 50)
+    vote_status = models.BooleanField() #True for upcote and False for downvote 
+    def __str__(self):
+        return self.name
+        
+
 class Question(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL ,on_delete = models.CASCADE)
     question = models.TextField()
     votes = models.SmallIntegerField(default = 0)
     added_date = models.DateTimeField()
+    voters = models.ManyToManyField(Voter,blank = True)
+    
     
     def __str__(self):
         return self.question
@@ -19,7 +28,7 @@ class Question(models.Model):
 
 class answer(models.Model):
     question = models.ForeignKey('forum.Question' ,on_delete = models.CASCADE, related_name = 'answer')
-    author = models.CharField(max_length=50)
+    author = models.CharField(max_length = 50)
     answer = models.TextField()
     votes = models.SmallIntegerField(default = 0)
     added_date = models.DateTimeField()
@@ -32,3 +41,4 @@ class answer(models.Model):
 
     def addTime(self):
         self.added_date = timezone.now()
+
