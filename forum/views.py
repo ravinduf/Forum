1ad8c0f939b4a8fdfe194e36075c2d64bc0ab0da
ravinduf <ravinduf@rocketmail.com>
2020-04-14 17:36:58 +0530

    fixed voting system

--- forum/views.py
+++ forum/views.py
@@ -27,15 +27,62 @@ def question_info(request, pk):
 
 def addVote(request,pk):
     question = get_object_or_404 (Question, pk=pk)
-    question.votes += 1
-    question.save()
-    return redirect('question_details', pk = question.pk) #you have to give the urlpattern name instead of view function and 
-                                                        #always remember to give simillar names for them
+    v = Votes.objects.filter(voter = request.user.username, question = question)
+    if v.count() == 0:
+        v = Votes.objects.create(question = question, voter = request.user.username)
+        v.status = True
+        v.save()
+        question.votes_count += 1
+        question.save()
+        return redirect('question_details', pk = question.pk) #you have to give the urlpattern name instead of view function and 
+                                                                #always remember to give simillar names for them
 
+    elif v.count() == 1:
+        v = Votes.objects.get(voter = request.user.username, question = question)
+        status = v.status
+        if status == False:
+            v.status = None
+            question.votes_count += 1
+            
+        elif status == None:
+            v.status = True
+            question.votes_count += 1
+
+        
+        v.save()
+        question.save()
+        return redirect('question_details', pk = question.pk)
+    
+    else:
+        return redirect('question_details', pk = question.pk)
+    
+        
+     
 def subVote(request,pk):
     question = get_object_or_404 (Question, pk=pk)
-    question.votes -= 1
-    question.save()
+    v = Votes.objects.filter(voter = request.user.username, question = question)
+    if v.count() == 0:
+        v = Votes.objects.create(question = question, voter = request.user.username)
+        v.status = False
+        v.save()
+        question.votes_count -= 1
+        question.save()
+        return redirect('question_details', pk = question.pk)
+    
+    elif v.count() == 1:
+        v = Votes.objects.get(voter = request.user.username, question = question)
+        status = v.status
+        if status == True:
+            v.status = None
+            question.votes_count -= 1
+        elif status == None:
+            v.status = False
+            question.votes_count -= 1
+        v.save()
+        question.save()
+        return redirect('question_details', pk = question.pk)
+    
+    
     return redirect('question_details', pk = question.pk)
 
 def addQuestion(request):
