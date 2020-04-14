81c3df4e718c6cc0686f5503cf4a465b812907a8
ravinduf <ravinduf@rocketmail.com>
2020-04-14 14:45:01 +0530

    deleted voter model

--- forum/models.py
+++ forum/models.py
@@ -2,11 +2,7 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 # Create your models here.
-class Voter(models.Model):
-    name = models.CharField(max_length = 50)
-    vote_status = models.BooleanField(blank = True, null = True) #True for upcote and False for downvote 
-    def __str__(self):
-        return self.name
+
         
 
 class Question(models.Model):
@@ -14,7 +10,7 @@ class Question(models.Model):
     question = models.TextField()
     votes = models.SmallIntegerField(default = 0)
     added_date = models.DateTimeField()
-    voters = models.ManyToManyField(Voter,blank = True)
+    
     
     
     def __str__(self):
