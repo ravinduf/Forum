f3b331839b384a9a7ab2c8aaf3f2d8563151d47e
ravinduf <ravinduf@rocketmail.com>
2020-04-21 11:35:46 +0530

    added more restrictions as question author cannot vote and answer to the same question

--- register/views.py
+++ register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import login, authenticate
 from .forms import RegisterForm
 
+
 # Create your views here.
 def register(request):
     if request.method == 'POST':
