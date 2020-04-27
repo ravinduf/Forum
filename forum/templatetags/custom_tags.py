d02e567116472bf36b1142e2afab4f3c2d344b86
ravinduf <ravinduf@rocketmail.com>
2020-04-27 17:53:37 +0530

    added custom tag len_dict

--- forum/templatetags/custom_tags.py
+++ forum/templatetags/custom_tags.py
@@ -5,4 +5,8 @@ register = template.Library()
 def get_item(dictionary, key):
     return dictionary.get(key)
 
+def len_dict(dictionary):
+    return len(dictionary)
+    
 register.filter('get_item', get_item)
+register.filter('len_dict', len_dict)
